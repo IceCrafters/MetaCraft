5d3cf0b077ae94fbc600ee9743f2994298227435
WithLithum <WithLithum@outlook.com>
2024-12-03 16:40:31 +0800

    :recycle: (locale) Remove FormattableString L overload
    It is so buggy and broken, compiler never autoselects it
    
    Signed-off-by: WithLithum <WithLithum@outlook.com>

--- src/MetaCraft/Lc.cs
+++ src/MetaCraft/Lc.cs
@@ -1,6 +1,8 @@
 // SPDX-FileCopyrightText: 2024 WithLithum <WithLithum@outlook.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+using JetBrains.Annotations;
+
 namespace MetaCraft;
 
 /// <summary>
@@ -21,19 +23,10 @@ internal static class Lc
     /// <summary>
     /// Acquires a string from the translation catalog.
     /// </summary>
-    /// <param name="formattable">The formatted string.</param>
-    /// <returns>The translated text.</returns>
-    internal static string L(FormattableString formattable)
-    {
-        return Application.Catalog.GetString(formattable);
-    }
-
-    /// <summary>
-    /// Acquires a string from the translation catalog.
-    /// </summary>
     /// <param name="format">The format string.</param>
     /// <param name="args">The args.</param>
     /// <returns>The translated text.</returns>
+    [StringFormatMethod(nameof(format))]
     internal static string L(string format, params object[] args)
     {
         return Application.Catalog.GetString(format, args);
