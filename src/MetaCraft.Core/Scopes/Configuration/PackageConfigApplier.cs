e01264484d5cb0f2f0b9cb81a6a3e983da1c8a56
WithLithum <WithLithum@outlook.com>
2024-12-12 19:06:49 +0800

    :recycle: (scope) Introduce new class for package configuration
    Signed-off-by: WithLithum <WithLithum@outlook.com>

---
+++ src/MetaCraft.Core/Scopes/Configuration/PackageConfigApplier.cs
@@ -0,0 +1,132 @@
+// SPDX-FileCopyrightText: 2024 WithLithum <WithLithum@outlook.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+using MetaCraft.Archive;
+using MetaCraft.Common.Platform;
+using MetaCraft.Core.Platform;
+using MetaCraft.Core.Transactions;
+using MetaCraft.Localisation;
+
+namespace MetaCraft.Core.Scopes.Configuration;
+
+public static class PackageConfigApplier
+{
+    public static void Apply(IPackageScope target,
+        PackageManifest manifest,
+        string packageRoot,
+        ITransactionAgent agent)
+    {
+        ExecuteConfigureScript(manifest, packageRoot, agent);
+        ProcessProjections(target, manifest, packageRoot, agent);
+        File.WriteAllText(Path.Combine(packageRoot, ".configured"), "CONFIGURED");
+    }
+    
+    private static void ExecuteConfigureScript(PackageManifest manifest,
+        string packageRoot,
+        ITransactionAgent agent)
+    {
+        // Find and execute configure scripts
+        var configurePath = Path.Combine(packageRoot, "config", "scripts", "install");
+        
+        // Doesn't need to run scripts if none
+        if (!Directory.Exists(configurePath)) return;
+        
+        if (!OperatingSystem.IsWindows()
+            && !OperatingSystem.IsLinux()
+            && !OperatingSystem.IsMacOS()
+            && !OperatingSystem.IsFreeBSD())
+        {
+            // No scripts will ever match, and even if they do, we don't have stuff to
+            // execute them
+            agent.PrintWarning(Lc.L("Package contains scripts but they are not supported on this platform"));
+            return;
+        }
+
+        // Find the file to execute.
+        var fileToExecute = GetInstallScriptExecute(configurePath);
+        if (fileToExecute == null)
+        {
+            return;
+        }
+
+        // Execute the file.
+        agent.PrintInfo(Lc.L("Running configure script for '{0}' ({1})...", manifest.Id, manifest.Version));
+        var exitCode = PlatformUtil.ExecuteBatch(fileToExecute);
+                
+        // On nonzero exit code
+        if (exitCode != 0)
+        {
+            throw new TransactionException(Lc.L("Configure script exited with exit code {0}.",
+                exitCode));
+        }
+    }
+    
+    private static string? GetInstallScriptExecute(string configurePath)
+    {
+        var exactPlatformFile = Path.Combine(configurePath, PlatformIdentifier.Current.ToString());
+        var anyArchitectureFile = Path.Combine(configurePath, new PlatformIdentifier(PlatformIdentifier.Current.System,
+            PlatformArchitecture.Any).ToString());
+        
+        if (File.Exists(exactPlatformFile))
+        {
+            return exactPlatformFile;
+        }
+
+        if (File.Exists(anyArchitectureFile))
+        {
+            return anyArchitectureFile;
+        }
+
+        return null;
+    }
+    
+    private static void ProcessProjections(IPackageScope target, PackageManifest manifest, string packageRoot,
+        ITransactionAgent agent)
+    {
+        // Handle projection.
+        var exports = manifest.Runtime?.ExportAssemblies;
+        if (exports == null) return;
+        
+        agent.PrintInfo(Lc.L("Processing projections for package '{0}' ({1})...",
+            manifest.Id, 
+            manifest.Version));
+        foreach (var export in exports)
+        {
+            var overwrite = false;
+                
+            // Get the projection source and see if it is an upgrade. If not, fail.
+            var source = target.Projection.GetProjectionSource(export);
+            if (source.HasValue)
+            {
+                if (source.Value.Name != manifest.Id
+                    || source.Value.Version.CompareSortOrderTo(manifest.Version) >= 1
+                    || !manifest.Unitary)
+                {
+                    throw new TransactionException(Lc.L("Projection '{0}' ({1}) already provided by '{2}' ({3})",
+                        export.Name,
+                        export.Version,
+                        source.Value.Name,
+                        source.Value.Version));
+                }
+
+                overwrite = true;
+            }
+
+            // Check for existence - this allows for a clearer error message.
+            var projectPath = Path.Combine(packageRoot, "contents", export.To);
+            if (!File.Exists(projectPath))
+            {
+                throw new TransactionException(Lc.L("Package '{0}' ({1}) tries to project file '{2}' which does not exist",
+                    manifest.Id,
+                    manifest.Version,
+                    export.To));
+            }
+                
+            // Insert the projection.
+            target.Projection.Insert(manifest, 
+                export,
+                projectPath,
+                overwrite);
+        }
+    }
+}
--- src/MetaCraft.Core/Transactions/PackageInstallTransaction.cs
+++ src/MetaCraft.Core/Transactions/PackageInstallTransaction.cs
@@ -5,6 +5,7 @@ using MetaCraft.Common.Platform;
 using MetaCraft.Core.Archive;
 using MetaCraft.Core.Platform;
 using MetaCraft.Core.Scopes;
+using MetaCraft.Core.Scopes.Configuration;
 using MetaCraft.Localisation;
 
 namespace MetaCraft.Core.Transactions;
@@ -35,112 +36,9 @@ public sealed class PackageInstallTransaction : ArgumentedTransaction<PackageIns
         agent.PrintInfo(Lc.L("Expanding package '{0}' ({1})...", package.Manifest.Id, package.Manifest.Version));
         package.ExpandArchive(location, Argument.Overwrite);
         
-        if (!ExecuteConfigureScript(package, agent, location)) return;
-        
-        // Handle projection.
-        var exports = package.Manifest.Runtime?.ExportAssemblies;
-        if (exports != null)
-        {
-            agent.PrintInfo(Lc.L("Processing projections for package '{0}' ({1})...",
-                package.Manifest.Id, 
-                package.Manifest.Version));
-            foreach (var export in exports)
-            {
-                var overwrite = false;
-                
-                // Get the projection source and see if it is an upgrade. If not, fail.
-                var source = Target.Projection.GetProjectionSource(export);
-                if (source.HasValue)
-                {
-                    if (source.Value.Name != package.Manifest.Id
-                        || source.Value.Version.CompareSortOrderTo(package.Manifest.Version) >= 1)
-                    {
-                        throw new TransactionException(Lc.L("Projection '{0}' ({1}) already provided by '{2}' ({3})",
-                            export.Name,
-                            export.Version,
-                            source.Value.Name,
-                            source.Value.Version));
-                    }
-
-                    overwrite = true;
-                }
-
-                // Check for existence - this allows for a clearer error message.
-                var projectPath = Path.Combine(location, "contents", export.To);
-                if (!File.Exists(projectPath))
-                {
-                    throw new TransactionException(Lc.L("Package '{0}' ({1}) tries to project file '{2}' which does not exist",
-                        package.Manifest.Id,
-                        package.Manifest.Version,
-                        export.To));
-                }
-                
-                // Insert the projection.
-                Target.Projection.Insert(package.Manifest, 
-                    export,
-                    projectPath,
-                    overwrite);
-            }   
-        }
-
-        File.WriteAllText(Path.Combine(location, ".configured"), "CONFIGURED");
-    }
-
-    private static bool ExecuteConfigureScript(PackageArchive package, ITransactionAgent agent, string location)
-    {
-        // Find and execute configure scripts
-        var configurePath = Path.Combine(location, "config", "scripts", "install");
-        
-        // Doesn't need to run scripts if none
-        if (!Directory.Exists(configurePath)) return true;
-        
-        if (!OperatingSystem.IsWindows()
-            && !OperatingSystem.IsLinux()
-            && !OperatingSystem.IsMacOS()
-            && !OperatingSystem.IsFreeBSD())
-        {
-            // No scripts will ever match, and even if they do, we don't have stuff to
-            // execute them
-            return false;
-        }
-
-        // Find the file to execute.
-        var fileToExecute = GetInstallScriptExecute(configurePath);
-        if (fileToExecute == null)
-        {
-            return false;
-        }
-
-        // Execute the file.
-        agent.PrintInfo(Lc.L("Running configure script for '{0}' ({1})...", package.Manifest.Id, package.Manifest.Version));
-        var exitCode = PlatformUtil.ExecuteBatch(fileToExecute);
-                
-        // On nonzero exit code
-        if (exitCode != 0)
-        {
-            throw new TransactionException(Lc.L("Configure script exited with exit code {0}.",
-                exitCode));
-        }
-
-        return true;
-    }
-
-    private static string? GetInstallScriptExecute(string configurePath)
-    {
-        var exactPlatformFile = Path.Combine(configurePath, PlatformIdentifier.Current.ToString());
-        var anyArchitectureFile = Path.Combine(configurePath, new PlatformIdentifier(PlatformIdentifier.Current.System,
-            PlatformArchitecture.Any).ToString());
-        
-        if (File.Exists(exactPlatformFile))
-        {
-            return exactPlatformFile;
-        }
-
-        if (File.Exists(anyArchitectureFile))
-        {
-            return anyArchitectureFile;
-        }
-
-        return null;
+        PackageConfigApplier.Apply(Target,
+            package.Manifest,
+            location,
+            agent);
     }
 }
